ee2ec2d9b36cf4c656e4a86607cfd0f466b5b18e
AdeleNikitina <nikitina.adeliya@gmail.com>
2023-03-22 14:53:00 +0300

    Ex3: Создание репозитория

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void TestHelloWorld(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+
+    @Test
+    public void TestHelloWorldEx3(){
+        System.out.println("Hello from Адель");
+    }
+
+}
