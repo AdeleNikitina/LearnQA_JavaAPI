f68069922f484ee02a0c24f40dc5ce2b3283fa82
AdeleNikitina <nikitina.adeliya@gmail.com>
2023-04-05 20:34:49 +0300

    Небольшие правки

--- src/test/java/TokenTest.java
+++ src/test/java/TokenTest.java
@@ -2,7 +2,8 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TokenTest {
 
