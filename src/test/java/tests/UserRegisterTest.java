0eee935093ac97d77be75feaa1247cfbd2582100
AdeleNikitina <nikitina.adeliya@gmail.com>
2023-04-16 17:35:25 +0300

    Ex15: Тесты на метод user

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -1,22 +1,33 @@
 package tests;
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import lib.Assertions;
 import lib.BaseTestCase;
 import lib.DataGenerator;
+import lib.ApiCoreRequests;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserRegisterTest extends BaseTestCase {
 
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
+    @Epic("Registry cases")
+    @Feature("Registration")
     @Test
-    public void testCreateUserWithExistingEmail(){
+    public void testCreateUserWithExistingEmail() {
         String email = "vinkotov@example.com";
 
-        Map<String,String> userData = new HashMap<>();
+        Map<String, String> userData = new HashMap<>();
         userData.put("email", email);
         userData = DataGenerator.getRegistrationData(userData);
 
@@ -30,9 +41,11 @@ public class UserRegisterTest extends BaseTestCase {
         Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
     }
 
+    @Epic("Registry cases")
+    @Feature("Registration")
     @Test
-    public void testCreateUserSuccessfully(){
-        Map<String,String> userData = DataGenerator.getRegistrationData();
+    public void testCreateUserSuccessfully() {
+        Map<String, String> userData = DataGenerator.getRegistrationData();
 
         Response responseCreateAuth = RestAssured
                 .given()
@@ -41,6 +54,89 @@ public class UserRegisterTest extends BaseTestCase {
                 .andReturn();
 
         Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
-        Assertions.assertJsonHasField(responseCreateAuth,"id");
+        Assertions.assertJsonHasField(responseCreateAuth, "id");
     }
+
+    @Epic("Registry cases")
+    @Feature("Registration")
+    @Test
+    @DisplayName("Negative test - check registration user with invalid email")
+    void testCreateUserWithInvalidEmail() {
+        String email = "example.com";
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user",
+                userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Invalid email format");
+    }
+
+    @Epic("Registry cases")
+    @Feature("Registration")
+    @ParameterizedTest
+    @ValueSource(strings = {"username", "email", "password", "firstName", "lastName"})
+    @DisplayName("Negative test - check registration user w/o required parameters")
+    void testCreateUserWithoutOneOfParameters(String condition) {
+        Map<String, String> userData = new HashMap<>();
+        userData = DataGenerator.getRegistrationData(userData);
+
+        if (condition.equals("username") || condition.equals("email") ||
+                condition.equals("password") || condition.equals("firstName") ||
+                condition.equals("lastName")) {
+            userData.remove(condition);
+        } else {
+            throw new IllegalArgumentException("Condition value is unknown: " + condition);
+        }
+
+        Response responseCreateAuth = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user",
+                userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The following required params are missed: " + condition);
+    }
+
+    @Epic("Registry cases")
+    @Feature("Registration")
+    @Test
+    @DisplayName("Negative test - check registration user w/o invalid name (1 symbol)")
+    void testCreateUserWithOneSymbolName() {
+        String username = "a";
+        Map<String, String> userData = new HashMap<>();
+        userData.put("username", username);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user",
+                userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'username' field is too short");
+    }
+
+    @Epic("Registry cases")
+    @Feature("Registration")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex1",
+            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea"
+    })
+    @DisplayName("Negative test - check registration user w/o invalid name (251 and more symbol)")
+    void testCreateUserWithMoreThen250SymbolName(String username) {
+        Map<String, String> userData = new HashMap<>();
+        userData.put("username", username);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user",
+                userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'username' field is too long");
+    }
+
 }
